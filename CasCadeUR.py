98423946ca51962cdbaeffd6c4da29822983e30c
Down <64877444+Downaaaa@users.noreply.github.com>
2023-11-03 15:40:45 +0800

    Update CasCadeUR.py

--- CasCadeUR.py
+++ CasCadeUR.py
@@ -6,11 +6,11 @@ import torch.utils.checkpoint as checkpoint
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 import torch.nn.functional as F
 from torchvision import models
-from Res import resnet50
+from Res import resnet18
 from torch.nn import init
 from torch.nn.parameter import Parameter
 # from unet_model import UNet
-from DecoderUnit import DecoderRES, MapEnhance, DecoderSegNext
+from DecoderUnit import DecoderRESF, MapEnhance, DecoderSegNext
 from BBRF import BBBNet2
 from Attention import HA
 # 这里ASPP的使用并不是传统的ASPP的使用方式，只有一个分支，并没有多个分支。
@@ -19,7 +19,7 @@ class BBBNet(nn.Module):
     def __init__(self, cfg=None):
         super(BBBNet, self).__init__()
         self.cfg = cfg
-        self.resnet = resnet50()
+        self.resnet = resnet18()
         # self.mscan = MSCAN(
         #     embed_dims=[64, 128, 320, 512],
         #     mlp_ratios=[8, 8, 4, 4],
@@ -28,9 +28,9 @@ class BBBNet(nn.Module):
         #     depths=[3, 5, 27, 3],
         #     norm_cfg=dict(type='BN', requires_grad=True))
         # self.mscan.load_state_dict(torch.load('/data/Hms/pth/mscan_l_20230227-cef260d4.pth')['state_dict'],strict=False)
-        self.resnet.load_state_dict(torch.load('/userHome/zy/Hms/BBRF-TIP-master/resnet50-19c8e357.pth'), strict=False)
+        self.resnet.load_state_dict(torch.load('/data/Hms/pth/resnet18-5c106cde.pth'), strict=False)
         #/userHome/zy/Hms/BBRF-TIP-master/resnet50-19c8e357.pth
-        self.decoderRes = DecoderRES(512, 1024, 2048)
+        self.decoderRes = DecoderRESF(64, 128, 256, 512)
         # self.decoderSeg = DecoderSegNext()
         # self.Fenhance1 = MapEnhance()
         # self.Fenhance2 = MapEnhance()
@@ -50,12 +50,13 @@ class BBBNet(nn.Module):
         # 需要注意一点其中的outmap是单通道的，outp是64通道的
         # feature1 = self.Fenhance1(outp, outmap)   # feature1做为下一个层级联结构的输入，所以要将UNet的结构的输入进行更改，现在Feature1的尺寸为H/2 W/2
         # feature1 = F.interpolate(feature1, size=x.shape[2:], mode='bilinear', align_corners=True)
-        feature1 = self.HA(torch.sigmoid(outmap), outp)
-        feature1 = F.interpolate(feature1, size=x.shape[2:], mode='bilinear', align_corners=True)
-        feature1_new = self.linear01(feature1)
-        input2 = torch.cat((feature1_new, feature1_new), 1)
-        input2 = torch.cat((feature1_new, input2), 1)   # 该部分输入到下一层的特征对于结果是有一定的影响的，个人认为直接64维度的输入好像要好一点，但是对于后续的改动很大
-        p2, p11, p12, p13, p14 = self.cas1(input2)
+        outmap = F.interpolate(outmap, size=x.shape[2:], mode='bilinear', align_corners=True)
+        feature1 = self.HA(torch.sigmoid(outmap), x)
+        # feature1 = F.interpolate(feature1, size=x.shape[2:], mode='bilinear', align_corners=True)
+        # feature1_new = self.linear01(feature1)
+        # input2 = torch.cat((feature1_new, feature1_new), 1)
+        # input2 = torch.cat((feature1_new, input2), 1)   # 该部分输入到下一层的特征对于结果是有一定的影响的，个人认为直接64维度的输入好像要好一点，但是对于后续的改动很大
+        p2, p11, p12, p13, p14 = self.cas1(feature1)
         # outfu, outmapu = self.cas1(feature1)
         # feature2 = self.Fenhance2(outfu, outmapu)
         #
